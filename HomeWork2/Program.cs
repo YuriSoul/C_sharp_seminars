9c68d098713e3f58679320bb013a804905ce0824
YuriSol <y.salokhin@mail.ru>
2022-06-27 20:00:11 +0300

    add new code

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -16,139 +16,55 @@ num = num % 10; //из оставшихся двух цифер отделяем
 
 Console.WriteLine(num + " ");
 
-
-
-//Метод заполняющая массив числами 
-/*void FillArray(int[]collection)
-{
-    int length = collection.Length; //Получаем длину массива
-    int index = 0; //Устанавливаем позицию индекса массива в начало
-
-    //Цикл для заполнения массива случайными цислами от 1 до 9
-    while (index < length)
-    {
-        collection[index] = new Random().Next(1, 10);
-        index++;
-    }
-}
-
-//Метод выводящий заполненный массив на печать
-void PrintArray(int[] coll)
-{
-    int count = coll.Length;
-    int position = 0;
-
-    while (position < count)
-    {
-        Console.Write(coll[position]);
-        position++;
-    }
-} 
-
-//Массив для хранения цифр трехзначного числа
-int[] array = new int[3];
-
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-
-Console.Write(array[1]);
-*/
-
-/*Задача 2: Напишите программу, которая выводит
-случайное трёхзначное число и удаляет вторую цифру
-этого числа.
-456 -> 46
-782 -> 72
-918 -> 98*/
-
-//Метод заполняющая массив числами 
-/*void FillArray(int[]collection)
-{
-    int length = collection.Length; //Получаем длину массива
-    int index = 0; //Устанавливаем позицию индекса массива в начало
-
-    //Цикл для заполнения массива случайными цислами от 1 до 9
-    while (index < length)
-    {
-        collection[index] = new Random().Next(1, 10);
-        index++;
-    }
-}
-
-//Метод выводящий заполненный массив на печать
-void PrintArray(int[] coll)
-{
-    int count = coll.Length;
-    int position = 0;
-
-    while (position < count)
-    {
-        Console.Write(coll[position]);
-        position++;
-    }
-} 
-
-//Массив для хранения цифр трехзначного числа
-int[] array = new int[3];
-
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-
-Console.Write(array[0]);
-Console.Write(array[2]);
-*/
-/*Задача 13: Напишите программу, которая выводит третью
-цифру заданного числа или сообщает, что третьей цифры
-нет.
+/*Задача 13: Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6*/
 
 int number;
- 
-Console.Write("Enter a three-digit number: ");
+
+Console.Write("Enter a number: ");
 number = Convert.ToInt32(Console.ReadLine());
 
-number = number / 10; //отсекаем крайнюю правую цифру числа, а оставшиеся две первых цифры 
-//сохраняем в num
-number = number % 10; //из оставшихся двух цифер отделяем крайнюю правую и выводим на печать 
-
-Console.WriteLine(num + " ");
-
-
-/*
-int num = 456;
-int n = num;
-int count = 0;
-int i = 1;
-Console.WriteLine(num + " ");
-while(n != 0)
-{
-    n = n / 10;
-    count++;
-
-    /*if(count >= 3)
+while(number >= 1000)
+{    
+    number = number / 10; 
+    
+    if(number >= 100 && number < 1000)
     {
-        num = num % 1000;
-        Console.Write(num);
-        break;
-    }*/
-/*}
+      break;
+    }   
+} 
 
-int[] array = new int[count];
-
-while(num != 0)
+if(number < 100)
 {
-    array[count-i] = num % 10;
-    num = num / 10; 
-    i++;  
-        
+    Console.WriteLine("There is no third number");      
+} 
+else 
+{
+    number = number % 10;
+    Console.WriteLine(number);
 }
-Console.Write(array[2]);
-*/
 
 
+/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели, и проверяет, является ли этот день выходным.
 
+6 -> да
+7 -> да
+1 -> нет*/
 
+int numDay;
+ 
+Console.Write("Enter the number of the day of the week: ");
+numDay = Convert.ToInt32(Console.ReadLine());
+
+if(numDay == 6 || numDay == 7)
+{
+   Console.Write(numDay + "th day is off day"); 
+}
+else
+{
+   Console.Write(numDay + "th day is work day"); 
+}
