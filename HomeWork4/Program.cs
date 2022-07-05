ba088e1fbc79dbb964ada3b33aba95857bbc83eb
YuriSol <y.salokhin@mail.ru>
2022-07-05 20:29:41 +0300

    add new code

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -35,7 +35,48 @@ void SumOfDigits(int num)
     }
 
     Console.Write("The sum of the digits in a number: "); //выводим заданное число
-    Console.Write(sum);
+    Console.WriteLine(sum);
 }
 Console.Write("Enter a number to sum its digits: ");
 SumOfDigits(Convert.ToInt32(Console.ReadLine()));
+
+
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и
+выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+//создаем массив из 8-и элементов
+
+//Метод заполняющая массив числами 
+void FillArray(int[]array)
+{
+    
+    //Цикл для заполнения массива случайными цислами 
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+//Метод выводящий массив на печать
+void PrintArray(int[]array)
+{
+    //Цикл для печати массива
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");        
+    }
+}
+
+//Создаем пустой массив из 8-и элементов
+int[] array = new int[8];
+
+//Наполняем массив числами
+FillArray(array);
+
+Console.Write("Source array: ");
+
+//Выводим массив на печать
+PrintArray(array);
