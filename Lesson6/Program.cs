9dcddc54b5cd66417d7c6420b4d8bd15e2bb62d1
YuriSol <y.salokhin@mail.ru>
2022-07-13 10:57:44 +0300

    add HomeWork6

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Метод переворачивающий массив наоборот
+int [] ReverseArray(int[] array)
+{
+    for (int i = 0, j = array.Length - 1; i < j; i++, j--)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+string ConvertDecNumToBinary(int num)
+{
+    string convNum = string.Empty;
+    while (num > 0)
+    {
+        convNum = num%2 + convNum;//остаток от деления исходного числа на 2 записываем в строку
+        num = num/2; //исходное число делим на 2 
+    }
+    return convNum;
+}
+Console.WriteLine(ConvertDecNumToBinary(10));
+
+//Метод выводящий массив на печать
+void PrintArray(int[]array)
+{
+    //Цикл для печати массива
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");        
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Source array: ");
+//Задаем массив
+int[] array = {1, 2, 3, 4, 5};
+
+//Выводим массив на печать
+PrintArray(array);
+//Переворачиваем массив
+ReverseArray(array);
+//Повторно выводим перевернутый массив на печать
+PrintArray(array);
+
+
