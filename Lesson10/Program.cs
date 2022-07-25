f7b4252cc133492e2d7615b108e67992d343f5b4
YuriSol <y.salokhin@mail.ru>
2022-07-25 13:10:07 +0300

    add code

--- Lesson10/Program.cs
+++ Lesson10/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Метод который принимает массив строк и
+//возвращает количество слов в которых больше 5-и символов
+
+int LongWordCounter(string[] words, int wordLength)
+{
+    int counter = 0;
+
+    for(int i = 0; i < words.Length; i++)
+    {
+        if(words[i].Length >= wordLength)
+        {
+            counter++;
+        } 
+        
+               
+    }
+    return counter;
+
+}
+int count  = 5;
+string[] names = {"Leonid", "Natalya", "Maxim", "Lubov", "Tom", "John", "Jan"};
+
+Console.WriteLine("Number of long words is " + LongWordCounter(names, count));
+
