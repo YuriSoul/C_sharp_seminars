11358f4deddae724664d2cd7a811e0c04ab5225f
YuriSol <y.salokhin@mail.ru>
2022-08-06 14:15:04 +0300

    Добавлен исходный код программы Program.cs

--- FinalHomeWork/Program.cs
+++ FinalHomeWork/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Метод выводящий строковый массив на печать
+void PrintArray(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");        
+    }
+    Console.WriteLine();
+}
+
+//Метод который принимает массив строк и
+//возвращает слова в которых 3 символа и меньше
+
+string[] FindStringsGivenLength(string[] words, int wordLength)
+{
+    int counter = 0;
+
+    for(int i = 0; i < words.Length; i++)
+    {
+        if(words[i].Length <= wordLength)
+        {
+            counter++;
+        }               
+    }
+
+    string[] names = new string[counter];
+
+    for(int i = 0, j = 0; i < words.Length; i++)
+    {
+        if(words[i].Length <= wordLength)
+        {   
+            names[j] = words[i];
+            j++;
+        }                
+    }
+    return names;
+}
+
+int wordLength  = 3;
+string[] words = {"Leonid", "Natalya", "Maxim", "Lubov", "Tom", "John", "Jan"};
+
+PrintArray(words);
+words = FindStringsGivenLength(words, wordLength);
+PrintArray(words);
